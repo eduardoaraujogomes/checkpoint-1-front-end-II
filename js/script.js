b612a947ae065639f11e6ede8c8024c48dde3bb7
Silvano AP <silvanoeng@hotmail.com>
2021-09-04 12:48:47 -0300

    faz nada com nada. kkkk

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// let formulario=document.querySelector("form");
+// formulario.addEventListener("Submit",function(event){});
+
+// event.preventDefault();
+// let campoNome = document.querySelector("input.nome");
+// if(campoNome.value==""){
+//     alert("O campo de nome não pode estar vazio.")
+// }
